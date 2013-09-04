565ac94f2590e5462d55be942f0e1719c9c6675e
Hack Reactor Students <github@hackreactor.com>
2013-09-04 16:00:27 -0700

    displaying thumbs and articles from reddit-no template

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,13 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+.controller("redditCtrl", ['$scope', '$http', function($scope, $http) {
+  $http({
+    method: 'JSONP',
+    url: 'http://api.reddit.com/r/hot.json?jsonp=JSON_CALLBACK'
+  }).success(function(data, status, headers, config) {
+    $scope.articles = data.data.children;
+    console.log('testing', data.data.children);
+    console.log('success');
+  }).error(function(data, status, headers, config) {
+    console.log('error');
+  });
+}]);
--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,4 @@
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+.directives();
+
+<a href="{{article.data.url}}"><img ng-src="{{article.data.thumbnail}}" /><br />{{ article.data.title }}</a>
