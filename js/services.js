db26ae8cd303a2219f6a4aaa4bfa6ebddf9e9362
Hack Reactor Students <github@hackreactor.com>
2013-09-05 17:22:54 -0700

    Moved reddit jsonp to a service

--- js/services.js
+++ js/services.js
@@ -1,1 +1,18 @@
-angular.module('myApp.services', []);
+angular.module('myApp.services', [])
+.factory('getReddit', ['$http', '$q', function($http, $q) {
+
+  return function() {
+    var d = $q.defer();
+      $http({
+        method: 'JSONP',
+        url: 'http://api.reddit.com/r/hot.json?jsonp=JSON_CALLBACK'
+      }).success(function(data, status, headers, config) {
+        d.resolve(data.data.children);
+        // console.log('success');
+      }).error(function(data, status, headers, config) {
+        // console.log('error');
+        d.reject(data);
+      });
+      return d.promise;
+  };
+}]);
