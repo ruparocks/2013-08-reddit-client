2e4337fc6f9b38248aeb9804e2da9748e0fa5460
Hack Reactor Students <github@hackreactor.com>
2013-09-05 17:23:57 -0700

    Added a filter to remove nsfw articles

--- js/filters.js
+++ js/filters.js
@@ -1,1 +1,13 @@
-angular.module('myApp.filters', []);
+angular.module('myApp.filters', [])
+.filter('sfw', function() {
+  return function(array) {
+    if (array) {
+      var articles = [];
+      angular.forEach(array, function(article) {
+        if (article.data.thumbnail !== 'nsfw')
+          articles.push(article);
+      });
+      return articles;
+    }
+  };
+});
